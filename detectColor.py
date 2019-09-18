74faf2fafcf0bf0e0cecaa13de5d817ac9495eca
Nick Polakrit <malimai.nick@gmail.com>
2019-09-18 18:16:06 +0700

    detect color

--- detectColor.py
+++ detectColor.py
@@ -12,15 +12,15 @@ args = vars(ap.parse_args())
 image = cv2.imread('./test.jpg')
 
 # define the list of boundaries
-boundaries = [
-    ([17, 15, 100], [50, 56, 200]),
-    ([105, 45, 0], [255, 165, 90]),
-    ([4, 154, 204], [92, 209, 255]),
-    ([103, 86, 65], [145, 133, 128])
-]
+boundaries = {
+    "red": ([17, 15, 100], [50, 56, 200]),  # เข้ม อ่อน
+    "blue":  ([105, 45, 0], [255, 165, 90]),
+    "yellow":  ([34, 121, 173], [109, 156, 180]),
+    "green": ([49, 104, 20], [127, 163, 115])
+}
 
 # loop over the boundaries
-for (lower, upper) in boundaries:
+for color_name, (lower, upper) in boundaries.items():
     # create NumPy arrays from the boundaries
     lower = np.array(lower, dtype="uint8")
     upper = np.array(upper, dtype="uint8")
@@ -29,6 +29,8 @@ for (lower, upper) in boundaries:
     # the mask
     mask = cv2.inRange(image, lower, upper)
     output = cv2.bitwise_and(image, image, mask=mask)
+    if mask.any():
+        print(f"{color_name}: {mask.sum()}")
 
     # show the images
     cv2.imshow("images", np.hstack([image, output]))
