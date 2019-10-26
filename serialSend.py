fda73fb0d4ae338c36b3fece0f4337ac08c64ff5
Mpolakrit <mpolakrit@MacBook-Pro-khxng-Mpolakrit.local>
2019-10-27 00:20:56 +0700

    serial

--- serialSend.py
+++ serialSend.py
@@ -1,4 +1,4 @@
 import serial
-ser = serial.Serial('/dev/cu.usbserial-AC00YIZF')  # open serial port
+ser = serial.Serial('/dev/cu.SOC')  # open serial port
 print(ser.name)         # check which port was really used
 ser.write(b'hello')
