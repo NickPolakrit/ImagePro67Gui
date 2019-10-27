4e202691da4840b68f1aa9f0054ba7f7d65400c0
Mpolakrit <mpolakrit@MacBook-Pro-khxng-Mpolakrit.local>
2019-10-27 09:49:31 +0700

    serial stage1 finish

--- serialSend.py
+++ serialSend.py
@@ -1,4 +1,17 @@
+# import serial
+# ser = serial.Serial('/dev/cu.usbserial-AC00YIZ6')  # open serial port
+# print(ser.name)         # check which port was really used
+# ser.write(b's')
+
+
 import serial
-ser = serial.Serial('/dev/cu.SOC')  # open serial port
-print(ser.name)         # check which port was really used
-ser.write(b'hello')
+serialDevice = serial.Serial(
+    "/dev/cu.usbserial-AC00YIZF", 115200, 8, 'N', 1, 0, 0, 0, 0, 0)
+
+serialDevice.setRTS(0)
+serialDevice.setDTR(0)
+
+while (1):
+    g = str(input("Commands : "))
+    print(g)
+    serialDevice.write(g.encode())
