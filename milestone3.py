5eb09085425a499a3038247696a9970bc4a2905f
Mpolakrit <mpolakrit@MacBook-Pro-khxng-Mpolakrit.local>
2019-11-12 22:33:24 +0700

    enable distance

--- milestone3.py
+++ milestone3.py
@@ -52,26 +52,26 @@ def safe_div(x, y):  # so we don't crash so often
     return x/y
 
 
-# def rescale_result(result, percent=80):  # make the video windows a bit smaller
-#     width = int(result.shape[1] * percent / 100)
-#     height = int(result.shape[0] * percent / 100)
-#     dim = (width, height)
-#     return cv2.resize(result, dim, interpolation=cv2.INTER_AREA)
+def rescale_result(result, percent=80):  # make the video windows a bit smaller
+    width = int(result.shape[1] * percent / 100)
+    height = int(result.shape[0] * percent / 100)
+    dim = (width, height)
+    return cv2.resize(result, dim, interpolation=cv2.INTER_AREA)
 
 
 if not cap.isOpened():
     print("can't open camera")
     exit()
 
-# windowName = "Result"
+windowName = "Result"
 
-# cv2.namedWindow(windowName)
-# cv2.createTrackbar("threshold", windowName, 75, 255, nothing)
-# cv2.createTrackbar("kernel", windowName, 5, 30, nothing)
-# cv2.createTrackbar("iterations", windowName, 1, 10, nothing)
+cv2.namedWindow(windowName)
+cv2.createTrackbar("threshold", windowName, 75, 255, nothing)
+cv2.createTrackbar("kernel", windowName, 5, 30, nothing)
+cv2.createTrackbar("iterations", windowName, 1, 10, nothing)
 
-# cv2.setTrackbarPos("threshold", windowName, 210)
-# cv2.setTrackbarPos("kernel", windowName, 13)
+cv2.setTrackbarPos("threshold", windowName, 210)
+cv2.setTrackbarPos("kernel", windowName, 13)
 # cv2.setTrackbarPos("iterations", windowName, 1)
 # Sliders to adjust image
 # https://medium.com/@manivannan_data/set-trackbar-on-image-using-opencv-python-58c57fbee1ee
@@ -123,112 +123,112 @@ while(showLive):
     # both = np.concatenate((frame, result), axis=1)
     # cv2.imshow('Frame', both)
 
-    # result_resize = rescale_result(resultWarp)
+    result_resize = rescale_result(resultWarp)
     if not ret:
         print("cannot capture the frame")
         exit()
 
-    # thresh = cv2.getTrackbarPos("threshold", windowName)
-    # ret, thresh1 = cv2.threshold(result_resize, thresh, 255, cv2.THRESH_BINARY)
+    thresh = cv2.getTrackbarPos("threshold", windowName)
+    ret, thresh1 = cv2.threshold(result_resize, thresh, 255, cv2.THRESH_BINARY)
 
-    # kern = cv2.getTrackbarPos("kernel", windowName)
-    # # square image kernel used for erosion
-    # kernel = np.ones((kern, kern), np.uint8)
+    kern = cv2.getTrackbarPos("kernel", windowName)
+    # square image kernel used for erosion
+    kernel = np.ones((kern, kern), np.uint8)
 
-    # itera = cv2.getTrackbarPos("iterations", windowName)
-    # dilation = cv2.dilate(thresh1, kernel, iterations=itera)
-    # # refines all edges in the binary image
-    # erosion = cv2.erode(dilation, kernel, iterations=itera)
+    itera = cv2.getTrackbarPos("iterations", windowName)
+    dilation = cv2.dilate(thresh1, kernel, iterations=itera)
+    # refines all edges in the binary image
+    erosion = cv2.erode(dilation, kernel, iterations=itera)
 
-    # opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
-    # closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
-    # closing = cv2.cvtColor(closing, cv2.COLOR_BGR2GRAY)
+    opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+    closing = cv2.cvtColor(closing, cv2.COLOR_BGR2GRAY)
 
     # find contours with simple approximation cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE
-    # contours, hierarchy = cv2.findContours(
-    #     closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(
+        closing, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    # closing = cv2.cvtColor(closing, cv2.COLOR_GRAY2RGB)
-    # cv2.drawContours(closing, contours, -1, (128, 255, 0), 1)
+    closing = cv2.cvtColor(closing, cv2.COLOR_GRAY2RGB)
+    cv2.drawContours(closing, contours, -1, (128, 255, 0), 1)
 
     # focus on only the largest outline by area
-    # areas = []  # list to hold all areas
+    areas = []  # list to hold all areas
 
-    # for contour in contours:
-    #     ar = cv2.contourArea(contour)
-    #     areas.append(ar)
+    for contour in contours:
+        ar = cv2.contourArea(contour)
+        areas.append(ar)
 
-    # try:
-    #     max_area = max(areas)
-    # except:
-    #     pass
-    # # index of the list element with largest area
+    try:
+        max_area = max(areas)
+    except:
+        pass
+    # index of the list element with largest area
 
-    # try:
-    #     max_area_index = areas.index(max_area)
-    # except:
-    #     pass
+    try:
+        max_area_index = areas.index(max_area)
+    except:
+        pass
 
-    # # largest area contour is usually the viewing window itself, why?
-    # try:
-    #     cnt = contours[max_area_index - 1]
-    # except:
-    #     pass
+    # largest area contour is usually the viewing window itself, why?
+    try:
+        cnt = contours[max_area_index - 1]
+    except:
+        pass
 
-    # cv2.drawContours(closing, [cnt], 0, (0, 0, 255), 4)
+    cv2.drawContours(closing, [cnt], 0, (0, 0, 255), 4)
 
-    # def midpoint(ptA, ptB):
-    #     return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+    def midpoint(ptA, ptB):
+        return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-    # # compute the rotated bounding box of the contour
-    # orig = result_resize.copy()
-    # box = cv2.minAreaRect(cnt)
-    # box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-    # box = np.array(box, dtype="int")
+    # compute the rotated bounding box of the contour
+    orig = result_resize.copy()
+    box = cv2.minAreaRect(cnt)
+    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    box = np.array(box, dtype="int")
 
-    # # order the points in the contour such that they appear
-    # # in top-left, top-right, bottom-right, and bottom-left
-    # # order, then draw the outline of the rotated bounding
-    # # box
-    # box = perspective.order_points(box)
-    # cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 4)
+    # order the points in the contour such that they appear
+    # in top-left, top-right, bottom-right, and bottom-left
+    # order, then draw the outline of the rotated bounding
+    # box
+    box = perspective.order_points(box)
+    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 4)
 
     # loop over the original points and draw them
-    # for (x, y) in box:
-    #     cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+    for (x, y) in box:
+        cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
 
-    # # unpack the ordered bounding box, then compute the midpoint
-    # # between the top-left and top-right coordinates, followed by
-    # # the midpoint between bottom-left and bottom-right coordinates
-    # (tl, tr, br, bl) = box
-    # (tltrX, tltrY) = midpoint(tl, tr)
-    # (blbrX, blbrY) = midpoint(bl, br)
+    # unpack the ordered bounding box, then compute the midpoint
+    # between the top-left and top-right coordinates, followed by
+    # the midpoint between bottom-left and bottom-right coordinates
+    (tl, tr, br, bl) = box
+    (tltrX, tltrY) = midpoint(tl, tr)
+    (blbrX, blbrY) = midpoint(bl, br)
 
-    # # compute the midpoint between the top-left and top-right points,
-    # # followed by the midpoint between the top-righ and bottom-right
-    # (tlblX, tlblY) = midpoint(tl, bl)
-    # (trbrX, trbrY) = midpoint(tr, br)
+    # compute the midpoint between the top-left and top-right points,
+    # followed by the midpoint between the top-righ and bottom-right
+    (tlblX, tlblY) = midpoint(tl, bl)
+    (trbrX, trbrY) = midpoint(tr, br)
 
     # compute the Euclidean distance between the midpoints
-    # dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-    # dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
 
-    # # compute the size of the object
-    # # more to do here to get actual measurements that have meaning in the real world
-    # pixelsPerMetric = 1
-    # dimA = dA / pixelsPerMetric
-    # dimB = dB / pixelsPerMetric
+    # compute the size of the object
+    # more to do here to get actual measurements that have meaning in the real world
+    pixelsPerMetric = 1
+    dimA = dA / pixelsPerMetric
+    dimB = dB / pixelsPerMetric
 
-    # # draw the object sizes on the image , text size card
-    # cv2.putText(orig, "{:.1f}mm".format(dimA), (int(
-    #     tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
-    # cv2.putText(orig, "{:.1f}mm".format(dimB), (int(
-    #     trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+    # draw the object sizes on the image , text size card
+    cv2.putText(orig, "{:.1f}mm".format(dimA), (int(
+        tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
+    cv2.putText(orig, "{:.1f}mm".format(dimB), (int(
+        trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (255, 255, 255), 2)
 
     # compute the center of the contour
-    # M = cv2.moments(cnt)
-    # cX = int(safe_div(M["m10"], M["m00"]))
-    # cY = int(safe_div(M["m01"], M["m00"]))
+    M = cv2.moments(cnt)
+    cX = int(safe_div(M["m10"], M["m00"]))
+    cY = int(safe_div(M["m01"], M["m00"]))
 
     # ------ COLOR ------------
     # Red color
@@ -476,9 +476,9 @@ while(showLive):
     # cv2.imshow('', closing)
     cv2.imshow('Mask All', result)
     # cv2.imshow('Color', closing)
-    # frame2 = np.concatenate((orig, closing), axis=1)
-    # cv2.imshow('window', frame2)
-    # cv2.imshow('result', orig)
+    frame2 = np.concatenate((orig, closing), axis=1)
+    cv2.imshow('window', frame2)
+    cv2.imshow('result', orig)
     cv2.imshow('Red', red)
     frame3 = np.concatenate((black, yellow), axis=1)
     # frame2 = np.concatenate((frame, red), axis=1)
